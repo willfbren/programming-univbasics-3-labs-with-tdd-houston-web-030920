c9b11c850dc6c2ecca5a44f3a689275833a7aecb
Will Brennan <willfbren@gmail.com>
2020-02-22 18:02:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,7 @@
 first_number = 3
 second_number = 1 
 
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
